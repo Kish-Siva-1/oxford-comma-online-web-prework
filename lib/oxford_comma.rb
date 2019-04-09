a7a4f722d257e31a00e4cc340c95083658c0be34
Kish Siva <rodes88@msn.com>
2019-04-09 21:52:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")  
   elsif array.length > 2
-    array.insert()
+    array.insert(,)
     array.join(", ")
     array.push()
     
